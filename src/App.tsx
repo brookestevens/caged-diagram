3a34021faf32bb75fc7395ea506f991e8b4af838
Brooke Stevens <brst4163@colorado.edu>
2025-11-01 17:56:20 -0600

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import type { ChangeEventHandler, ChangeEvent } from 'react';
-import './App.css'
+import Metronome from './Metromone';
 
 type Note = string;
 
@@ -66,7 +66,7 @@ const getRomanNumeral = (degree:number, type:string) => {
 
 const getNotesFromMode = (key:Note, mode:number) => {
   const intervals = [
-    {interval: 2, type: 'm'},
+    {interval: 2, type: 'm'}, // major 3 has 4 semitoones, minor 3rd has 3
     {interval: 2, type: 'm'},
     {interval: 1, type: 'M'},
     {interval: 2, type: 'M'},
@@ -93,7 +93,8 @@ const getNotesFromMode = (key:Note, mode:number) => {
   return scale;
 }
 
-function knuthShuffle(array) {
+function knuthShuffle(arr) {
+  let array = [...arr];
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
@@ -101,65 +102,83 @@ function knuthShuffle(array) {
   return array;
 }
 
-const generateRandomInterval = () => {
-  const num = Math.floor(Math.random() * 3);
-  if (num === 0) {
-    return '1'
-  }
-  if (num === 1) {
-    return '3';
-  }
-  if (num === 2) {
-    return '5';
-  }
-}
+const shuffledNotes = knuthShuffle(notes);
 
 function App() {
   const [key, setKey] = useState(notes[0]);
   const [mode, setMode] = useState(0);
   const [view, setView] = useState('interval');
   const [scale, setScale] = useState([]);
-  const [highlight, setHighlight] = useState(0);
-  const [progression, setProgression] = useState([]);
+  const [highlight, setHighlight] = useState([]);
+  const [activeRow, setActiveRow] = useState(0);
+  const [note, setActiveNote] = useState(0);
 
   useEffect(() => {
     const _scale = getNotesFromMode(key, mode);
     setScale(_scale);
   }, [key, mode]);
 
+  const shouldHighlight = (degree:number) :string => {
+    let classes = '';
+    if (highlight.includes(degree)) {
+      classes += 'highlight-' + degree;
+    }
+    return classes;
+  }
+
   function renderRows() {
-    const renderTd = (heading:string, degrees:Array<Array<number>>) => {
+    const renderTd = (heading:string, degrees:Array<Array<number>>, rowNum:number) => {
       return (
-        <tr key={heading}>
+        <tr onClick={() => setActiveRow(rowNum)} className={activeRow === rowNum ? 'bg-black' : ''} key={heading}>
           <th> {heading}</th>
           {degrees.map((degree:Array<number>, index:number) =>
-            <td key={`${heading}-${index}`} className={`${degree.includes(highlight) ? 'highlight' : ''}`} >
+            <td key={`${heading}-${index}`} >
             { view === 'interval' ?
-              degree.map((d) => getRomanNumeral(d, scale[d].type)).join(' / ') : 
-              degree.map((d) => scale[d].note ).join(' / ') }
+              degree.map((d) => <span className={shouldHighlight(d)}> {getRomanNumeral(d, scale[d].type)} </span>) : 
+              degree.map((d) => <span className={shouldHighlight(d)}> {scale[d].note } </span>) }
             </td>
           )}
         </tr>
       );
     }
     const row = [];
-    modes.forEach((m) => {
+    modes.forEach((m, index) => {
       // Get the shifted shape.
       const offset = 7 - mode;
       const { shapes } = modes[(m.degree + offset) % 7];
-      row.push(renderTd(m.name, shapes));
+      row.push(renderTd(m.name, shapes, index));
     });
     return row;
   }
 
-  function generateProgression() {
-    const arr = knuthShuffle([...scale]);
-    setProgression(arr);
+  function handleNoteClick(index: number) {
+    setHighlight((prevState) => {
+      const i = highlight.indexOf(index);
+      if (i > -1) {
+        const newState = [...prevState];
+        newState.splice(i, 1);
+        return newState;
+      }
+      return [...prevState, index];
+    })
+  }
+
+  function handleMetromoneClick(isClick: boolean) {
+    if (isClick) {
+      setActiveRow(Math.floor(Math.random() * 7));
+      setHighlight([Math.floor(Math.random() * 7)]);
+      setActiveNote(prevState => {
+        return (prevState + 1) % shuffledNotes.length;
+      })
+    }
   }
 
   return (
     <div>
-      <h1> CAGED Shapes in Scale Shapes</h1>
+      <Metronome onMetronomeClick={handleMetromoneClick}/>
+      <div>
+        <h1> Note: { shuffledNotes[note] }</h1>
+      </div>
       <div className='flex'>
         <NoteSelector selected={key} onSelect={(e: ChangeEvent) => setKey((e.target as HTMLSelectElement).value) } />
         <ModeSelector selected={mode} onSelect={(e: ChangeEvent) => setMode(parseInt((e.target as HTMLSelectElement).value, 10)) }/>
@@ -191,18 +210,12 @@ function App() {
         <ul>
           {scale.length > 0 && scale.map((s, index) =>
           <li 
-            onClick={() => setHighlight(index)}
-            className={`${highlight === index ? 'highlight' : ''}`}
+            onClick={() => handleNoteClick(index)}
+            className={shouldHighlight(index)}
             key={s.note}>
               {s.note + ' - ' + getRomanNumeral(index, s.type) }
             </li >)}
         </ul>
-      </div>
-      <div>
-        <button onClick={generateProgression}> generate chord progression </button>
-        <div>
-          { progression.map((degree) => <li key={degree.note}> {degree.note} | {generateRandomInterval()} </li>) }
-        </div>
       </div>
     </div>
   )
